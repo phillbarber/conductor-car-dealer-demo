c06a5bfe6a3e98f80832a9c09379d6be9b893087
Phill Barber <phillbarber@github.com>
2023-10-31 22:48:07 +0000

    Not sure why this is broken

--- car-dealer/src/test/java/com/github/phillbarber/conductor/EndToEndTest.kt
+++ car-dealer/src/test/java/com/github/phillbarber/conductor/EndToEndTest.kt
@@ -4,6 +4,7 @@ import org.junit.jupiter.api.Assertions.assertTrue
 import org.junit.jupiter.api.Test
 import org.testcontainers.containers.FixedHostPortGenericContainer
 import org.testcontainers.containers.GenericContainer
+import org.testcontainers.containers.Network
 import org.testcontainers.junit.jupiter.Container
 import org.testcontainers.junit.jupiter.Testcontainers
 import org.testcontainers.utility.DockerImageName
@@ -53,19 +54,27 @@ class EndToEndTest {
 
      */
 
-    @Container
-    var redis = GenericContainer(DockerImageName.parse("redis:5.0.3-alpine")).withExposedPorts(6379)
+    var network = Network.newNetwork()
 
     @Container
-    var elasticSearch = GenericContainer(DockerImageName.parse("lasticsearch:6.8.15"))
+    var redis = GenericContainer(DockerImageName.parse("redis:5.0.3-alpine"))
+        .withExposedPorts(6379)
+        .withNetwork(network)
+        .withNetworkAliases("rs")
+
+    @Container
+    var elasticSearch = GenericContainer(DockerImageName.parse("elasticsearch:6.8.15"))
         .withEnv("ransport.host", "0.0.0.0")
         .withEnv("discovery.type", "single-node")
         .withEnv("xpack.security.enabled", "false")
         .withEnv("ES_JAVA_OPTS", "-Xms512m -Xmx1024m")
         .withExposedPorts(9200, 9300)
+        .withNetwork(network)
+        .withNetworkAliases("es")
 
-    @Container
-    var conductorServer = GenericContainer(DockerImageName.parse("conductor:server")).withExposedPorts(8080)
+
+//    @Container
+//    var conductorServer = GenericContainer(DockerImageName.parse("conductor:server")).withExposedPorts(8080).withNetwork(network)
 
 
 
@@ -78,6 +87,9 @@ class EndToEndTest {
 
         System.out.println("NICE")
         System.out.println("${redis.host} ")
+        System.out.println("${elasticSearch.host} ")
+        System.out.println(" ${redis.firstMappedPort} ")
+        System.out.println(" ${elasticSearch.firstMappedPort} ")
         System.out.println(" ${redis.firstMappedPort} ")
         System.out.println("YAY")
         Thread.sleep(10000)
